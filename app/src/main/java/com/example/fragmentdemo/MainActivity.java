97913858f59126da95daf1bb9ac6e154da35eddb
teddyzhang1976 <teddy@fic-sh.com.cn>
2015-12-08 21:27:02 +0800

    add more comment

--- app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -12,6 +12,34 @@ import android.view.View.OnClickListener;
 import com.example.fragment.Fragment1;
 import com.example.fragment.Fragment2;
 
+// life cycle 1: normally installed and start
+// 1. this.onCreate
+	//1.1 here, we set fragment layout from xml on setContentView
+	//1.2 we create fragment object.
+	//1.3 we connect the object to fragment layout container we set in 1.1 by issuing transaction.
+	//1.4 How about when we assign an class in fragment layout xml in 1.1?
+// 2. this.onStart
+// 3. then Fragment1.onAttach
+// 4. then this.onAttachFragment
+// 5. then Fragment1.onCreate()
+// 6. then Fragment1.onCreateView()
+// 7. then Fragment1.onActivityCreated
+// 8. then Fragment1.onStart();
+// 9. then this.onResume();
+// 10. then Fragment1.onResume();
+// 11. When stitched out or screen is locked off....
+// 12. this.onPause
+// 13. Fragment1.onPause
+// 14. this.onStop
+// 15. Fragment1.onStop
+// 16. this.onRestart
+// 17. this.onStart
+// 18. Fragment1.onStart
+// 19. this.onResume
+// 20. Fragment1.onResume
+// 21. Goto 1.
+
+
 @SuppressLint("Recycle")
 public class MainActivity extends FragmentActivity {
 
@@ -81,26 +109,7 @@ public class MainActivity extends FragmentActivity {
 		Log.i(TAG, "onStart");
 		super.onStart();
 		Log.i(TAG,"onStart is going outside");
-		//before onResume
-		// next step is Fragment1.onAttach
-		// then this.onAttachFragment
-		// then Fragment1.onCreate()
-		// then Fragment1.onCreateView()
-		// then Fragment1.onActivityCreated
-		// then Fragment1.onStart();
-		// then this.onResume();
-		// then Fragment1.onResume();
-		// 1. When stitched out or screen is locked off....
-		// this.onPause
-		// Fragment1.onPause
-		// this.onStop
-		// Fragment1.onStop
-		// this.onRestart
-		// this.onStart
-		// Fragment1.onStart
-		// this.onResume
-		// Fragment1.onResume
-		// Goto 1.
+
 
 	}
 
