a04d382618528ccfc86d96b8f15b986b8a745203
teddyzhang1976 <teddy@fic-sh.com.cn>
2015-12-09 17:09:03 +0800

    Add some comments

--- app/src/main/java/com/example/fragment/Fragment1.java
+++ app/src/main/java/com/example/fragment/Fragment1.java
@@ -3,6 +3,7 @@ package com.example.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,10 +19,25 @@ public class Fragment1 extends Fragment {
 		 void onArticleSelected(int result);
 	}
 
+	/**
+	 * 1. First callback when create object from Activity.setContentView().
+	 * What can we do here?
+	 */
+	@Override
+	public void onInflate(Activity activity, AttributeSet attrs,Bundle savedInstanceState){
+		Log.v(TAG, "Fragment1 onInflate");
+		super.onInflate(activity,attrs,savedInstanceState);
+		//super.onInflate(Activity activity, AttributeSet attrs, Bundle savedInstanceState);
+	}
+
+	/**
+	 * 2. Second callback when first time associated to an activity. no longer called later.
+	 * What can we do here?
+	 */
 	@Override
 	public void onAttach(Activity activity) {
 		Log.v(TAG, "Fragment1 onAttach");
-		Bundle args = getArguments();
+		Bundle args = getArguments();//is this can be called when we need it? ,I think it is.
 		if (null != args) {
 			/*
 			 * ���������ǿ��Խ���Acivity�յ������ݱ���������������ʾ��Fragment���С�
@@ -33,65 +49,109 @@ public class Fragment1 extends Fragment {
 		super.onAttach(activity);
 	}
 
+	/**
+	 * 3. 3rd callback. after onAttach()
+	 * what can we do here?
+	 * we got a savedInstanceState here , so the function is used to restore saved args for the fragment. Just like activity.onCreate().
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		Log.v(TAG, "Fragment1 onCreate");
 		super.onCreate(savedInstanceState);
 	}
 
-	@Override
-	public void onDestroy() {
-		Log.v(TAG, "Fragment1 onDestroy");
-		super.onDestroy();
-	}
-
-	@Override
-	public void onResume() {
-		Log.v(TAG, "Fragment1 onResume");
-		super.onResume();
-	}
-
-	@Override
-	public void onStart() {
-		Log.v(TAG, "Fragment1 onStart");
-		super.onStart();
-	}
-
-	@Override
-	public void onDetach() {
-		Log.v(TAG, "Fragment1 onDetach");
-		super.onDetach();
-	}
-
-	@Override
-	public void onPause() {
-		Log.v(TAG, "Fragment1 onPause");
-		super.onPause();
-	}
-
-	@Override
-	public void onStop() {
-		Log.v(TAG, "Fragment1 onStop");
-		super.onStop();
-	}
-
+	/**
+	 * 4. 4th callback
+	 * what can we do here? return a view(group) to system that will connect it to it's container.
+	 * System will call this when it's time to connect fragment's layout to activity's container
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+							 Bundle savedInstanceState) {
 		Log.v(TAG, "Fragment1 onCreateView");
 		return inflater.inflate(R.layout.fragment1, container, false);
 	}
 
+	/**
+	 *
+	 */
 	@Override
-	public void onDestroyView() {
-		Log.v(TAG, "Fragment1 onDestroyView");
-		super.onDestroyView();
+	public void onViewCreated(View view, Bundle savedInstanceState){
+		Log.v(TAG, "Fragment1 onViewCreated");
+		super.onViewCreated(view,savedInstanceState);
 	}
 
+	/**
+	 * 5. 5th callback. When system call activity.onStart, it will call it inside this function.
+	 * what can we do here.
+	 * This called means activity has finished the windows creation, we can process input controller here from now on.
+	 */
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		Log.v(TAG, "Fragment1 onActivityCreated");
 		super.onActivityCreated(savedInstanceState);
 	}
 
+	/**
+	 * 6. 6th callback. in activity.onStart()
+	 */
+	@Override
+	public void onStart() {
+		Log.v(TAG, "Fragment1 onStart");
+		super.onStart();
+	}
+
+	/**
+	 * 7. 7th callback. in activity.onResume(). Last callback when fragment is shown to user. Now, fragment is ready to work~~~
+	 */
+	@Override
+	public void onResume() {
+		Log.v(TAG, "Fragment1 onResume");
+		super.onResume();
+	}
+
+	/**
+	 * 8. 8th callback. after activity.onPause()
+	 */
+	@Override
+	public void onPause() {
+		Log.v(TAG, "Fragment1 onPause");
+		super.onPause();
+	}
+
+	/**
+	 * 9. 9th callback, fragment windows will disapear from screen.
+	 */
+	@Override
+	public void onStop() {
+		Log.v(TAG, "Fragment1 onStop");
+		super.onStop();
+	}
+
+	/**
+	 * 10. 10th callback
+	 */
+	@Override
+	public void onDestroyView() {
+		Log.v(TAG, "Fragment1 onDestroyView");
+		super.onDestroyView();
+	}
+
+	/**
+	 * 11. 11th callback
+	 */
+	@Override
+	public void onDestroy() {
+		Log.v(TAG, "Fragment1 onDestroy");
+		super.onDestroy();
+	}
+
+	/**
+	 * 12. 12th callback
+	 */
+	@Override
+	public void onDetach() {
+		Log.v(TAG, "Fragment1 onDetach");
+		super.onDetach();
+	}
 }
--- app/src/main/java/com/example/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -12,32 +12,75 @@ import android.view.View.OnClickListener;
 import com.example.fragment.Fragment1;
 import com.example.fragment.Fragment2;
 
-// life cycle 1: normally installed and start
-// 1. this.onCreate
-	//1.1 here, we set fragment layout from xml on setContentView
-	//1.2 we create fragment object.
-	//1.3 we connect the object to fragment layout container we set in 1.1 by issuing transaction.
-	//1.4 How about when we assign an class in fragment layout xml in 1.1?
-// 2. this.onStart
-// 3. then Fragment1.onAttach
-// 4. then this.onAttachFragment
-// 5. then Fragment1.onCreate()
-// 6. then Fragment1.onCreateView()
-// 7. then Fragment1.onActivityCreated
-// 8. then Fragment1.onStart();
-// 9. then this.onResume();
-// 10. then Fragment1.onResume();
-// 11. When stitched out or screen is locked off....
-// 12. this.onPause
-// 13. Fragment1.onPause
-// 14. this.onStop
-// 15. Fragment1.onStop
-// 16. this.onRestart
-// 17. this.onStart
-// 18. Fragment1.onStart
-// 19. this.onResume
-// 20. Fragment1.onResume
-// 21. Goto 1.
+/*
+life cycle 1: normally installed and start without Fragment1 layout declaration in activity_main.
+1. this.onCreate(){
+	1.1 here, we set fragment layout from xml on setContentView
+	1.2 we create fragment object.
+	1.3 we connect the object to fragment layout container we set in 1.1 by issuing transaction.
+	1.4 How about when we assign an class in fragment layout xml in 1.1?
+}
+2. this.onStart（）{
+	2.1. Fragment1.onAttach
+		3.1
+	2.2. this.onAttachFragment
+	2.3. Fragment1.onCreate()
+	2.4. Fragment1.onCreateView()
+	2.5. Fragment1.onActivityCreated
+	2.6. Fragment1.onStart();
+}
+
+3. this.onResume();
+4. Fragment1.onResume();
+5. this.onPause(){
+	5.1 Fragment1.onPause
+}
+6. this.onStop(){
+	6.1. Fragment1.onStop
+}
+7. this.onRestart
+8. this.onStart(){
+	8.1. Fragment1.onStart
+}
+9. this.onResume
+10. Fragment1.onResume
+
+11. Goto 5.
+*/
+
+/*
+life cycle 2: normally installed and start with Fragment1 layout declaration in activity_main.
+1. this.onCreate(){
+//by issuing setContentView with fragment layout in it. System will immediately create an object of the fragment and call its callback function step by step.
+	1.1. Fragment1.onInflate
+	1.2. Fragment1.onAttach
+		1.2
+	1.3. this.onAttachFragment
+	1.4. Fragment1.onCreate()
+	1.5. Fragment1.onCreateView()
+}
+2. this.onStart（）{
+	2.1. Fragment1.onActivityCreated
+	2.2. Fragment1.onStart();
+}
+
+3. this.onResume();
+4. Fragment1.onResume();
+5. this.onPause(){
+	5.1 Fragment1.onPause
+}
+6. this.onStop(){
+	6.1. Fragment1.onStop
+}
+7. this.onRestart
+8. this.onStart(){
+	8.1. Fragment1.onStart
+}
+9. this.onResume
+10. Fragment1.onResume
+
+11. Goto 5.
+*/
 
 
 @SuppressLint("Recycle")
@@ -47,22 +90,32 @@ public class MainActivity extends FragmentActivity {
 	private Fragment1 fragment1;
 	private Fragment2 fragment2;
 
+	/**
+	 *
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.i(TAG, "onCreate");
 		super.onCreate(savedInstanceState);
+		/**
+		 * here, activity_main contained a fragment element declaration, so when we call setContentView , the system immediately create an object of fragment1 and call its onInflate() call back.
+		 * The object is now locked in the layout. And onAttach(),onCreate .... callbacks are conscequently called as normal.
+		 */
 		setContentView(R.layout.activity_main);
+
+		/*
 		fragment1 = new Fragment1(); //Seems no callback is called on this step!
 		fragment2 = new Fragment2();
 		Bundle args = new Bundle();
 		args.putString("hello", "Hello Fragment1");
-		fragment1.setArguments(args);
+		fragment1.setArguments(args);//Here args is set to a private variable of fragment1
 		Log.i(TAG, "create Fragment1");
 		FragmentTransaction transaction = getSupportFragmentManager()
 				.beginTransaction();
 		transaction.replace(R.id.main, fragment1);
 		transaction.addToBackStack(null);
 		transaction.commit();
+		*/
 		Log.i(TAG, "fragment1 is added to main layout, but still no call back is triggled");
 		findViewById(R.id.btn).setOnClickListener(new OnClickListener() {
 			@Override
@@ -78,50 +131,55 @@ public class MainActivity extends FragmentActivity {
 				transaction.commit();
 			}
 		});
+		Log.i(TAG, "onCreate End");
 	}
 
 	@Override
 	public void onAttachFragment(Fragment fragment) {
 		Log.i(TAG, "onAttachFragment");
 		super.onAttachFragment(fragment);
+		Log.i(TAG, "onAttachFragment End");
 	}
 
 	@Override
 	protected void onDestroy() {
 		Log.i(TAG, "onDestroy");
 		super.onDestroy();
+		Log.i(TAG, "onDestroy End");
 	}
 
 	@Override
 	protected void onPause() {
 		Log.i(TAG, "onPause");
 		super.onPause();
+		Log.i(TAG, "onPause End");
 	}
 
 	@Override
 	protected void onResume() {
 		Log.i(TAG, "onResume");
 		super.onResume();
+		Log.i(TAG, "onResume End");
 	}
 
 	@Override
 	protected void onStart() {
 		Log.i(TAG, "onStart");
 		super.onStart();
-		Log.i(TAG,"onStart is going outside");
-
-
+		Log.i(TAG,"onStart End");
 	}
 
 	@Override
 	protected void onStop() {
 		Log.i(TAG, "onStop");
 		super.onStop();
+		Log.i(TAG, "onStop End");
 	}
 
 	@Override
 	protected void onRestart() {
 		Log.i(TAG, "onRestart");
 		super.onRestart();
+		Log.i(TAG, "onRestart End");
 	}
 }
