1106cbf922be71c1626ab0d120d44f325b3a7aa4
teddyzhang1976 <teddyzhang1976@gmail.com>
2017-10-07 15:53:21 +0800

    fix build error

--- app/MIMI/app/src/main/java/com/example/teddy/ui/MainActivity.java
+++ app/MIMI/app/src/main/java/com/example/teddy/ui/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.teddy.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.teddy.system.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
