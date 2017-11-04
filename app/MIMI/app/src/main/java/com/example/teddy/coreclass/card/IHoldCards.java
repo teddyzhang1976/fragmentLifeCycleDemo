d1e039d440cc5bdd298ddae465a0e7f06e37a2f9
teddyzhang1976 <teddyzhang1976@gmail.com>
2017-11-04 09:18:42 +0800

    add holdcards package and class

---
+++ app/MIMI/app/src/main/java/com/example/teddy/coreclass/card/IHoldCards.java
@@ -0,0 +1,8 @@
+package com.example.teddy.coreclass.card;
+
+/**
+ * Created by teddy on 2017/11/4.
+ */
+
+public interface IHoldCards {
+}
---
+++ app/MIMI/app/src/main/java/com/example/teddy/coreclass/card/holdcards/HoldCardsQ6s.java
@@ -0,0 +1,10 @@
+package com.example.teddy.coreclass.card.holdcards;
+
+import com.example.teddy.coreclass.card.IHoldCards;
+
+/**
+ * Created by teddy on 2017/11/4.
+ */
+
+public class HoldCardsQ6s implements IHoldCards {
+}
